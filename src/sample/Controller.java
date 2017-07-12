a1a04de1a4b5aab3f80ce6ec7cb62eca53d7fb75
hblolj <hblolj@gmail.com>
2017-07-12 22:18:59 +0800

    The First Commit.This is a Demo That MenuItem By Right Click.

---
+++ src/sample/Controller.java
@@ -0,0 +1,55 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+
+    private Main main;
+    private Stage stage;
+
+    @FXML GridPane gridPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gridPane.setOnMouseClicked(event ->{
+            if (event.getButton() == MouseButton.SECONDARY){
+                System.out.println("右键点击事件触发了哦!");
+                ContextMenu menu = new ContextMenu();
+                MenuItem item1 = new MenuItem("第一项");
+                MenuItem item2 = new MenuItem("第二项");
+                MenuItem item3 = new MenuItem("第三项");
+                item1.setOnAction(event1 -> {
+                    System.out.println("点击了第一项");
+                });
+                item2.setOnAction(event1 -> {
+                    System.out.println("点击了第二项");
+                });
+                item3.setOnAction(event1 -> {
+                    System.out.println("点击了第三项");
+                });
+                menu.getItems().add(item1);
+                menu.getItems().add(item2);
+                menu.getItems().add(item3);
+                menu.show(stage, event.getScreenX(), event.getScreenY());
+            }
+        });
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    public void setStage(Stage s){
+        this.stage = s;
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = fxmlLoader.load();
+        Controller controller = fxmlLoader.getController();
+        Scene scene = new Scene(root, 300, 275);
+        controller.setMain(this);
+        controller.setStage(primaryStage);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
